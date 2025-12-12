da535af2efbe442532a421dd39ec2a05c7d9f0e0
HerosAugustoBaiaOliveira <herosbaia2006@gmail.com>
2025-12-12 20:31:02 -0300

    Estrutura inicial corrigida

---
+++ trabalho.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+#include "cabecalho.h"
+
+int main() {
+    setlocale(LC_ALL, "Portuguese");
+    printf("baguio doido\n");
+    return 0;
+}
