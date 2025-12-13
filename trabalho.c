57cd41bed4595a9d5dd80b14ea605197d3827857
Joke17 <jjsoprofissional@gmail.com>
2025-12-13 16:08:09 -0300

    git bugadasso

--- trabalho.c
+++ trabalho.c
@@ -6,15 +6,11 @@
 
 int main() {
     setlocale(LC_ALL, "Portuguese");
-<<<<<<< HEAD
     AlterarDadosPaciente();
 
     IndexPaciente i;
     i.posicao = 10;
 
     printf("baguio doido\n");
-=======
-    printf("baguios doido\n");
->>>>>>> a8377eb243a153487b4de5f6276ccbe493a89786
     return 0;
 }
