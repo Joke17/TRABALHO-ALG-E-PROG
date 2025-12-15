fdbc205be8e53bd06dd0bb4db98970e3dbc6424d
ajota10 <afrfj@aluno.ifnmg.edu.br>
2025-12-14 21:21:10 -0300

    FUNCIONOU PORRA

--- pacientes.c
+++ pacientes.c
@@ -57,7 +57,7 @@ void CarregarIndicePacientes(){
     fread(vetPacientes, sizeof(Paciente), quantidade, ptpacientes);
 
     for(int i = 0; i < quantidade; i++){
-        vetIndex[i] = vetPacientes[i];
+        strcpy(vetIndex[i].chave, vetPacientes[i].nome);
     }
     for(int i = 0; i < quantidade; i++){
         printf("%s \n", vetIndex[i].chave);
