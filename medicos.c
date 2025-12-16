91ff61c26b31327f0f60ffd8e46d91ae4646ed41
ajota10 <afrfj@aluno.ifnmg.edu.br>
2025-12-15 21:19:12 -0300

    getchar

--- medicos.c
+++ medicos.c
@@ -144,7 +144,8 @@ m.especialidade[strcspn(m.especialidade, "\n")] = 0;
 
     printf("Valor Hora (ex: 100.50): ");
     scanf("%f", &m.valor_hora_trabalho); // Lê o float
-    getchar(); // Limpa buffer novamente
+    getchar(); // Limpa o \n do buffer
+    //while ((c = getchar()) != '\n' && c != EOF);
 
     // --- Passo 2: Gravar os DADOS no arquivo (HDD) ---
     // Abre o arquivo de DADOS em modo append ("ab"), adiciona ao final
